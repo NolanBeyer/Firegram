4e45ed90ef2537ff8b43b34afb534049299fa7b2
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-11-27 00:46:33 -0800

    Created three components and a way to upload files

---
+++ src/components/UploadForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const UploadForm = () => {
+  const [file, setFile] = useState(null)
+  const [error, setError] = useState(null)
+
+  const types = ['image/png', 'image/jpeg']
+  function changeHandler(e) {
+    let selected = e.target.files[0]
+    if (selected && types.includes(selected.type)) {
+      setFile(selected)
+      setError('')
+    } else {
+      setFile(null)
+      setError('Pleas select an image file (png or jpeg)')
+    }
+  }
+  return (
+    <form>
+      <label>
+        <input type='file' onChange={changeHandler} />
+      </label>
+      <div className='output'>
+        {error && <div className='error'>{error}</div>}
+        {file && <div>{file.name}</div>}
+      </div>
+    </form>
+  )
+}
+
+export default UploadForm
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from './firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB9jXkOmG_lLRoEFfeI234s79x4rrMh5nc',
+  authDomain: 'firegram-a7301.firebaseapp.com',
+  projectId: 'firegram-a7301',
+  storageBucket: 'firegram-a7301.appspot.com',
+  messagingSenderId: '903781819683',
+  appId: '1:903781819683:web:1e5fa6d54581c42dfd1896',
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const projectsStorage = firebase.storage()
+const projectFirestore = firebase.firestore()
+
+export { projectsStorage, projectFirestore }
