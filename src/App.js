2b91dfd7b6d7f56c30cfeac4870cf2579fa21aa6
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-11-27 16:40:42 -0800

    Created modal component and added handleClicks to close out of it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from './components/Title'
 import UploadForm from './components/UploadForm'
 import ImageGrid from './components/ImageGrid'
-function App() {
+import Modal from './components/Modal'
+
+const App = () => {
+  const [selectedImg, setSelectedImg] = useState(null)
   return (
     <div className='App'>
       <Title />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   )
 }
